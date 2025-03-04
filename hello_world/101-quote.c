4f2604de87fb3cc3610f0e97481b821b4de1b6de
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-04 07:49:32 +0000

    write using write function

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: Porgram print string using write function
+ *
+ *Return: (Alwasy 0 when success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
+	return(0);
+}		
