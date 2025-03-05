c4e29c5fbe855f8cb52b582c905e051d398fe6cb
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-05 19:36:22 +0000

    print with string

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main  - Entry point
+ * Description: print strinf using loop & putchar
+ *
+ *Return: Always 0 when success
+ */
+int main(void)
+{
+	char string[] = "_putchar";
+	int length  = sizeof(string);
+	int i = 0;
+
+	while (i < length)
+	{
+		putchar(string[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
