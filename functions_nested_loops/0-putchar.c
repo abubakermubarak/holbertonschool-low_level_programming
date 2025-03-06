a1b82499bb87617afeeb6cd3a3656f24314aa9f7
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-06 07:26:31 +0000

    making _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main  - Entry point
  * Description: print strinf using loop & putchar
@@ -8,15 +8,8 @@
  */
 int main(void)
 {
-	char string[] = "_putchar";
+	char string[] = "_putchar\n";
 	int length  = sizeof(string);
-	int i = 0;
-
-	while (i < length)
-	{
-		putchar(string[i]);
-		i++;
-	}
-	putchar('\n');
+	write(1, string, length);
 	return (0);
 }
