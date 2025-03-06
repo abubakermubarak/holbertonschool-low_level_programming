e6d4f013537dee85e7455f939642da72bdc03cb3
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-06 11:21:31 +0000

    Hot fix

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,11 +4,12 @@
  * print_last_digit - return last digit of a number
  * Description: Return the Reminder of the number divided by 10
  * @n : Any number
- * Return : The reminder of number divided by 10
+ *Return: The reminder of number divided by 10
  */
 int print_last_digit(int n)
 {
-	int last_digit = abs((n % 10)) + 48;
-	_putchar(last_digit);
-	return last_digit;
+	int last_digit = abs((n % 10));
+
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
