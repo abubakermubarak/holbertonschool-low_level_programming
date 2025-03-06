edc6c88f461729ba411a79103f510dc168e8b769
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-06 19:40:22 +0000

    upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _isupper(int c) - Check if the letter is upper
+ * _isupper - Check if the letter is upper
  * Decription: Compare the letter with "A" and "Z" if its = or between
+ * @c: Any charcter
  * Return: 1 if upper 0 otherwise
  */
- int _isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
