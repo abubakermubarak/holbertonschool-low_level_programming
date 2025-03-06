9b1cacc691ee65b2d0d891c069367f97cc5c174d
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-06 08:12:26 +0000

    print the alpabet in lower X10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lower
+ * Description: Print the aplabet in lower X10
+ *
+ * Return : None
+ */
+void print_alphabet(void);
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
