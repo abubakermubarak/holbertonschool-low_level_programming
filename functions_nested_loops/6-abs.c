ae2fcd579f4f5c470a055422c2d3e4ca56e39931
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-06 09:23:18 +0000

    Return abs value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Return the absoltue value
+ * Description: if postive return it if negative X-1 and return it
+ * @n : Any number
+ * Return: The absoulte value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+	return (n);
+}
