c441bc0878234a40478f8e2d503e56cc13bb6e9d
root <abubakermubarak>
2025-03-08 09:23:17 +0000

    Addtion

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Addition function
+ * @a : Any integer
+ * @b : Any integer
+ * Return: The addtion of Two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
