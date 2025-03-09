d33721c5b47976538b00903e1ea4524f370a15c3
abubkermubarak <abubkermubarak95@gmail.com>
2025-03-09 11:52:35 +0000

    Fibonacci

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Print first 50 fib number
+ *
+ * Return: Always 0 when success
+ */
+int main(void)
+{
+	/* intialize variables */
+	long prev = 0;
+	long old = 1;
+	long cur;
+	int i;
+	/* Itrate 50 times */
+	for (i = 0; i <= 50; i++)
+	{
+		/* fib num equals the summ of the previous two numbers */
+		cur = prev + old;
+		printf("%lu", cur);
+		if (i < 50)
+		{
+			printf(", ");
+		}
+		old = prev;
+		prev = cur;
+	}
+	printf("\n");
+	return (0);
+}
