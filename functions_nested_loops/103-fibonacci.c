0d7ff627c1604b9c6b11b0270f52722e69e1a7c2
abubkermubarak <abubkermubarak95@gmail.com>
2025-03-09 15:51:39 +0000

    sum of odd fib nums under 4m

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -3,14 +3,13 @@
  * main - Entry point
  * Description : print sum of odd term in fib numbers under 4b
  *
- * Return : Always 0 when success
+ * Return: (Always 0 when success)
  */
 int main(void)
 {
-	/* intialize variables */
 	int sum = 0;
 	int prev = 0;
-       	int old = 1;
+	int old = 1;
 	int cur = 1;
 	/* itrate untill 4 million */
 	while (cur <= 4000000)
@@ -26,4 +25,4 @@ int main(void)
 	}
 	printf("%d\n", sum);
 	return (0);
-}	
+}
