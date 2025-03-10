5f0c4d234a8d48c6fd62fdc37921d174ed4f0fba
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-10 12:46:35 +0000

    Hot fix

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,24 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if(n <= 0)
 	{
-		for (j = 0; j < n; j++)
-		{
-			if (i == j)
-			{
-				_putchar('\\');
-				break;
-			}
-			_putchar(' ');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i == j)
+				{
+					_putchar('\\');
+					break;
+				}
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
 	}
 }
