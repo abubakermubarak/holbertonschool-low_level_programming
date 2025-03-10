3fda112c7f4a1c7e2ec60e4244b79289b331c34e
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-10 07:46:49 +0000

    Multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mult - multiply two digit
+ * @a: Any Integer
+ * @b: Any integer
+ * Return: The result of multiplication 
+ */
+int mul(int a, int b)
+{
+	return ( a * b);
+}
