91f0bb6d199d81fa5a804cfa1fc7832ebdec3b32
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-18 07:55:15 +0000

    combination

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - combination of two digits
+ * Description: possible different combinations of two digits.
+ *
+ * Rerturn: 0 when success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j > i)
+			{	
+				putchar('0' + i);
+				putchar('0' + j);
+			
+				if (i < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
