7d4351c11b699df9b349a0a771666b3eecf74a56
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-19 10:13:04 +0000

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - fizz buzz
+ * Description: print numbers from 0-99
+ *
+ * Return: None
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i > 0 && i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i > 0 && i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i > 0 && i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%i", i);
+			if (i < 99)
+			{
+				printf(" ");
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
