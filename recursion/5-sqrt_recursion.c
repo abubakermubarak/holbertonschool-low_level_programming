4fc3546dcff7fb822637fc11615c61fac236857a
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-22 13:50:28 +0000

    find squareroot using recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+int find_sqr(int n, int i);
+/**
+ * _sqrt_recursion - print squre root
+ * Description - print squre root of number using recusrion
+ * @n : Any number
+ * Return: -1 if number doesnot have squre root otherwise the square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (n);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (find_sqr(n, 1));
+}
+/**
+ * find_srq - check for squre root
+ * Description:checks if i * i equlas n
+ * @n: integer to find square root of
+ * @i: number to check if it's squre = n
+ * Return: square root if found otherwise -1
+ */
+int find_sqr(int n, int i)
+{
+	if ( i * i == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	return find_sqr(n, i + 1)
+}
