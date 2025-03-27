3214821b26685831f2ad9852d4f80bfabdb0e9fa
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-27 10:56:58 +0000

    Handle NULL

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,20 +13,21 @@ char *str_concat(char *s1, char *s2)
 {
 	int l1, l2, i, j;
 	char *string;
+	/* if any string is NULL make it empty */
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	/* calculate two strings length */
 	for (l1 = 0; s1[l1] != '\0'; l1++)
 	{
 	}
 	for (l2 = 0; s2[l2] != '\0'; l2++)
 	{
-	}
-	if (s1 == NULL)
-	{
-		return (s2);
-	}
-	if (s2 == NULL)
-	{
-		return (s1);
 	}
 	string = malloc(l1 + l2 + 1);
 	if (string == NULL)
