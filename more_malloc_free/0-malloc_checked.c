55526d7a4c245d068bf92d7fca7e3dcdeb2e9149
abubakermubarak <abubakermubarak95@gmail.com>
2025-03-30 10:08:12 +0000

    hotfix

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -8,9 +8,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	if (malloc(b) == NULL)
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	return (malloc(b));
+	return (ptr);
 }
