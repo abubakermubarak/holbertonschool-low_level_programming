73fc78f96a32494e4f00424ce5b0542182469904
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-03 10:11:47 +0000

    dog structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ * struct dog - define object dog
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ *
+ * the structure define object dog with propites
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+/**
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+*/
 #endif
