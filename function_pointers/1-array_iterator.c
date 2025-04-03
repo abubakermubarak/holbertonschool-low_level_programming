18388820340f939af3f92ccef814e376993329e4
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-03 13:10:21 +0000

    hot fix

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
  * array_iterator - executes function on elements
- * Description: executes a function given as a parameter on each element of an array.
+ * Description: executes a function on each element of an array.
  * @array: pointer to array of integers
  * @size: size of the array
  * @action: a pointer to the function you need to use
