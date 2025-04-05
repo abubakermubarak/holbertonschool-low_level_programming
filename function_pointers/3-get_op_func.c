3a2590ab56c07d70b9d054aa192c492c073f0934
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-05 12:41:28 +0000

    number 3 function pointer

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+	int i;
+	
+	i = 0;	
+    while (i < 6)
+    {
+	    if (ops[i][0] != NULL)
+	    {
+		    return (&ops[i][1]);
+	    }
+	    i++;
+    }
+    return (NULL);
+
+}
