f0ae0fa9ff5303c4bafbd71c7748eb9cf98e6594
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-06 09:46:40 +0000

    hot fix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,19 +17,19 @@ void print_all(const char * const format, ...)
 			case 'i':
 			{
 				i = va_arg(ap, int);
-				printf("%i", i);
+				printf("%i ", i);
 				break;
 			}
 			case 'c':
 			{
 				c = va_arg(ap, int);
-				printf("%c", c);
+				printf("%c ", c);
 				break;
 			}
 			case 'f':
 			{
 				f = va_arg(ap, double);
-				printf("%f", f);
+				printf("%f ", f);
 				break;
 			}
 			case 's':
@@ -37,11 +37,11 @@ void print_all(const char * const format, ...)
 				s = va_arg(ap, char *);
 				if (s == NULL)
 				{
-					printf("(nil)");
+					printf("(nil) ");
 				}
 				else
 				{
-					printf("%s", s);
+					printf("%s ", s);
 				}
 				break;
 			}
