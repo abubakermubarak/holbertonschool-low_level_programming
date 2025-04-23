2a08365a0ab14327ab6d91840b439d5e9b7fae74
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-23 12:52:03 +0000

    change head  value

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -19,5 +19,6 @@ list_t *add_node(list_t **head, const char *str)
     new_node->str = strdup(str);
     new_node->len = i;
     new_node->next = *head;
+    *head = new_node;
     return (new_node);
 }
