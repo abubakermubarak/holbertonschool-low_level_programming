799f0a3287c0c8fc49f4555e86f58e0ae8151c7f
abubakermubarak <abubakermubarak95@gmail.com>
2025-06-27 18:52:39 +0000

    fixing betty

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -23,10 +23,10 @@ int _atoi(char *s)
 	while (s[i])
 	{
 		/*handle sign*/
-        if(s[i] == '-')
-        {
-            sign *= -1;
-        }
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			num = num * 10 + (s[i] - '0');
