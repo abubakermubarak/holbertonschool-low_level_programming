24e6f89471ba71435c2dc8605416efbec7e8e046
abubakermubarak <abubakermubarak@gmail.com>
2025-07-03 22:43:53 +0000

    Remove test

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -43,13 +43,3 @@ char *cap_string(char *string)
 	}
 	return (string);
 }
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
-}
