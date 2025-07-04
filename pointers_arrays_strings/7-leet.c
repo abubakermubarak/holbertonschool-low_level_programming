cc5ffc35c647e783aeb31fdac668cb6147af1b79
abubakermubarak <abubakermubarak95@gmail.com>
2025-07-04 17:48:39 +0000

    encode string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - encode string
+ * Description: encode string into 1337
+ * @s: Any string
+ * Return: encoded string
+ */
+char *leet(char *s)
+{
+	int i;
+	int c;
+	/* encoding letters*/
+	char letters[] = {'A', 'E', 'O', 'T', 'L'};
+	int cipher[] = {4, 3, 0, 7, 1};
+
+	c = 0;
+	while (s[c])
+	{
+		if ((s[c] >= 'a' && s[c] <= 'z') || (s[c] >= 'A' && s[c] <= 'Z'))
+		{
+			for (i = 0; letters[i] != '\0'; i++)
+			{
+				if ((string[c] == letters[i]) || (string[c] - 32 == letters[i]))
+				{
+					string[c] =  '0' + cipher[i];
+				}
+			}
+		}
+		c++;
+	}
+	return (s);
+}
