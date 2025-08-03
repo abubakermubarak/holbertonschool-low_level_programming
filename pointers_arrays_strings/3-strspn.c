7250426a20e7ee1a252aca730b1a1d870d3b1d16
abubakermubarak <abubakermubarak95@gmail.com>
2025-08-03 12:07:22 +0000

    Adding flag to break when character not found

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,24 +1,30 @@
 #include "main.h"
 /**
- * _strspn - return number of bytes in the initial segment 
- * Description: number of bytes in the initial segment 
+ * _strspn - return number of bytes in the initial segment
+ * Description: number of bytes in the initial segment
  * @s: intial string
  * @accept: a prefix substring.
- * return: number of bytes in the initial segment of s 
+ * Return: number of bytes in the initial segment of s
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i,j, count;
+	unsigned int i, j, count, flag;
 
 	count = 0;
 	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		flag = 0
+		for (j = 0; s[j] != '\0' && flag == 0; j++)
 		{
 			if (accept[i] == s[j])
 			{
 				count++;
+				flag = 1;
 			}
+		}
+		if (flag == 0)
+		{
+			break;
 		}
 	}
 	return (count);
